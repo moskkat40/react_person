5dc63dfc2c4f707f3be1c7e58365734a7c706b49
Kateryna <moskkat40@gmail.com>
2024-09-20 09:24:23 +0200

    add healper variables

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,16 @@
 export const Person = ({
   person: { name, age, sex, isMarried, partnerName },
-}) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {name}</h2>
-    {!age ? '' : <p className="Person__age">I am {age}</p>}
-    {!isMarried ? (
-      <p className="Person__partner"> I am not married </p>
-    ) : (
-      <p className="Person__partner">
-        {partnerName} is my {sex === 'f' ? 'husband' : 'wife'}
-      </p>
-    )}
-  </section>
-);
+}) => {
+  const partnerStatus = sex === 'f' ? 'husband' : 'wife';
+  const PartnerDescription = isMarried
+    ? `${partnerName} is my ${partnerStatus}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{PartnerDescription}</p>
+    </section>
+  );
+};
